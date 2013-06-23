751e0870666e52ec00c544c0d0d61ac1feb85265
chemdemo <yangdemo@gmail.com>
2013-06-24 02:47:14 +0800

    dev log

--- web/js/edit.js
+++ web/js/edit.js
@@ -1,1 +1,5 @@
-define();
+define(['lib/ace/ace', 'lib/pagedown/Markdown.Converter'], function(ace) {
+	var editor = ace.edit('post-editor');
+	editor.setTheme('lib/ace/theme/chrome');
+	editor.getSession().setMode("lib/ace/mode/markdown");
+});
