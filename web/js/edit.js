58c89980c96b54c47a6cd6575d7f6d8554d595ad
chemdemo <yangdemo@gmail.com>
2013-06-25 01:30:22 +0800

    log 0625

--- web/js/edit.js
+++ web/js/edit.js
@@ -1,6 +1,6 @@
 define(['lib/ace/ace', 'lib/pagedown/Markdown.Converter'], function(ace) {
 	var editor = ace.edit('post-editor');
 	editor.setShowPrintMargin(false);
-	editor.setTheme('lib/ace/theme/chrome');
+	editor.setTheme('lib/ace/theme/github');
 	editor.getSession().setMode("lib/ace/mode/markdown");
 });
