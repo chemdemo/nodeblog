ef98ec4da49edb6344c58cae9e857882c44d8d44
chemdemo <yangdemo@gmail.com>
2013-09-09 21:04:23 +0800

    细节调整

--- server/models/post.js
+++ server/models/post.js
@@ -4,19 +4,19 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var PostSchema = new Schema({
-	title: {type: String, unique: true},
-	create_at: {type: Date, default: Date.now, index: true},
-	update_at: {type: Date, default: Date.now},
-	author_id: ObjectId,
-	content: String,
-	cover: String,
-	summary: String,
-	tags: [String],
-	comments: {type: Number, default: 0},
-	visite: {type: Number, default: 0},
-	topped: {type: Number, default: 0},
-	last_comment_at: {type: Date},
-	last_comment_by: ObjectId
+	title: {type: String, unique: true}
+	, create_at: {type: Date, default: Date.now, index: true}
+	, update_at: {type: Date, default: Date.now}
+	, author_id: ObjectId
+	, content: String
+	, cover: String
+	, summary: String
+	, tags: [String]
+	, comments: {type: Number, default: 0}
+	, visite: {type: Number, default: 0}
+	, topped: {type: Number, default: 0}
+	, last_comment_at: {type: Date}
+	, last_comment_by: ObjectId
 }, {collection: 'post'});
 
 PostSchema.index({create_at: 1});
--- server/models/user.js
+++ server/models/user.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-	name: String,
-	email: {type: String, unique: true},
-	pass: String,
-	avatar: String,// 允许自定义头像
-	site: String,
-	admin: {type: Boolean, default: false},
-	create_at: {type: Date, default: Date.now},
-	modify_at: {type: Date, default: Date.now}
+	name: String
+	, email: {type: String, unique: true}
+	, pass: String
+	, avatar: String// 允许自定义头像
+	, site: String
+	, admin: {type: Boolean, default: false}
+	, create_at: {type: Date, default: Date.now}
+	, modify_at: {type: Date, default: Date.now}
 }, {collection: 'user'});
 
 mongoose.model('User', UserSchema);
