9283969984d3373a7ae80d05b86195cb8702b748
chemdemo <yangdemo@gmail.com>
2013-09-12 18:14:12 +0800

    add config file

---
+++ settings.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+//process.env.NODE_ENV = 'development';
+//process.env.NODE_ENV = 'production';
+
+module.exports = {
+	NAME: 'Notes·dm'
+	, VERSION: '0.1.1'
+
+	, DB_HOST: 'localhost'
+	, DB_PORT: 27017
+	, DB_NAME: 'nodeblog'
+	, DB_USER: 'dm'
+	, DB_PASS: 'idm007'
+	, EXPIRES: 1000 * 60 * 60 * 24 * 30
+	, SESSION_PORT: 6379
+	, SESSION_SECRET: 'nodeblog@chemdemo$007'
+	, APP_PORT: 3000
+	, UPLOAD_DIR: path.resolve(__dirname, '/tmp')
+
+	, DEFAULT_USER_PASS: 'pass@123'
+	, ADMIN: {
+		NAME: 'chemdemo'
+		, EMAIL: 'yangdemo@gmail.com'
+		, PASS: 'demo2007'
+		, SITE: 'http://www.dmfeel.com'
+	}
+	, DEFAULT_AVATAR: '/style/images/avatar.png'
+
+	, ITEM_PER_PAGE: 20
+	, RCODES: {
+		SUCCESS: 0
+		, AUTH_ERROR: 10000
+		, PARAM_MISSING: 10001
+		, PARAM_ILLEGAL: 10002
+		, DB_ERROR: 10003
+	}
+}
