1b64a53ccc840eb2102afececab67ffff44f282b
chemdemo <yangdemo@gmail.com>
2013-09-14 18:56:31 +0800

    stop signup

--- server/routes/index.js
+++ server/routes/index.js
@@ -52,8 +52,8 @@ function routes(app) {
 	app.get('/home', home);
 	
 	// user about
-	app.get('/signup', sign.signup);
-	//app.post('/signup', sign.signup); 
+	//app.get('/signup', sign.signup);
+	//app.post('/signup', sign.signup);
 	app.get('/login', sign.login);
 	app.post('/login', sign.login);
 	app.get('/info', sign.info);
