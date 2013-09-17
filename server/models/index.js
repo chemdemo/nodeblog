d3465a4f45b50592ea61307b9cc6ed2a53dfa5c4
chemdemo <yangdemo@gmail.com>
2013-09-17 10:27:48 +0800

    bug fix at db connection module

--- server/models/index.js
+++ server/models/index.js
@@ -2,12 +2,20 @@ var settings = require('../../settings');
 var mongoose = require('mongoose');
 var db_url = 'mongodb://' + settings.DB_HOST + ':' + settings.DB_PORT + '/' + settings.DB_NAME;
 
-mongoose.connect(db_url, function(err) {
+// see http://stackoverflow.com/questions/11928151/mongoose-output-the-error-error-connection-closed
+// "mongoose.connect() Is not accepting any callback functions"
+/*mongoose.connect(db_url, function(err) {
 	if(!err) {
 		console.log('Connect to db success!' + '\ndb_url: ' + db_url);
 	} else {
 		console.error('Connect to db error.\ndb_url: '+db_url+'.\nError message: ' + err.message);
 	}
+});*/
+mongoose.connect(db_url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connect to db error: '));
+db.once('open', function() {
+	console.log('Connect to db success.\ndb_url:', db_url);
 });
 
 require('./user');
--- web/js/index.js
+++ web/js/index.js
@@ -1,19 +1,6 @@
 'use strict';
 
-require(['jquery', 'marked', 'hljs', 'utils'], function($, marked, hljs, utils) {
-	marked.setOptions({
-		highlight: function (code, lang) {
-			if(lang) {
-				return hljs.highlight(lang, code).value;
-			}
-			return hljs.highlightAuto(code).value;
-		},
-		breaks: true,
-		pedantic: true,
-		sanitize: false,
-		smartypants: true
-	});
-
+require(['jquery'], function($) {
 	function init() {
 		;
 	}
