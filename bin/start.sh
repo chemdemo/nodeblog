4434f9844243f4ff10155eac1d05973cec4de4e4
dm <yangdemo@gmail.com>
2013-09-19 01:09:30 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BLOG=$(forever list | grep /data/sites/www.dmfeel.com/app.js | awk '{print $5}')
+BLOG=$(forever list | grep [dir of app run script] | awk '{print $5}')
 
 if [ -z $BLOG ];
 then
 	echo "blog forever starting ..."
-	cd /data/sites/www.dmfeel.com
+	cd [dir of app]
 	# if [ ! -d logs/ ];then mkdir logs
-	NODE_ENV=production forever start --spinSleepTime 10000 -o logs/out.log -e logs/err.log -a /data/sites/www.dmfeel.com/app.js
+	NODE_ENV=production forever start --spinSleepTime 10000 -o logs/out.log -e logs/err.log -a [dir of app run script]
 	D=$(date "+%Y-%m-%d %H:%M:%S")
 	echo "forever started blog at "$D
 else
