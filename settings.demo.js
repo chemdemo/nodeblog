15c3347f8a8051762fb31b410e490d560b81c176
chemdemo <yangdemo@gmail.com>
2013-11-14 00:28:52 +0800

    update settings demo

--- settings.demo.js
+++ settings.demo.js
@@ -1,38 +1,44 @@
 var path = require('path');
 
-//process.env.NODE_ENV = 'development';
-//process.env.NODE_ENV = 'production';
+// process.env.NODE_ENV = 'development';
+// process.env.NODE_ENV = 'production';
 
 module.exports = {
-    NAME: '[NAME]'
-    , VERSION: '0.1.1'
-
-    , DB_HOST: '[DB_HOST]'
-    , DB_PORT: ['DB_PORT']// default 27017
-    , DB_NAME: '[DB_NAME]'
-    , DB_USER: '[DB_USER]'
-    , DB_PASS: '[DB_PASS]'
+    APP_PORT: [PORT]
+    , DB_HOST: [HOST]
+    , DB_PORT: [DB_PORT]
+    , DB_NAME: 'blog'
+    , DB_USER: [DB_NAME]
+    , DB_PASS: [DB_PASS]
     , EXPIRES: 1000 * 60 * 60 * 24 * 30 // 30 days
-    , SESSION_PORT: [SESSION_PORT]
-    , SESSION_SECRET: '[SESSION_SECRET]'
-    , APP_PORT: [APP_PORT]
-    , UPLOAD_DIR: path.resolve(__dirname, '/tmp')
+    , SESSION_PORT: [REDIS_PORT]
+    , SESSION_SECRET: [SESSION_SECRET]
+    , COOKIE_KEY: [COOKIE_KEY]
+    , COOKIE_SECRET: [COOKIE_SECRET]
+    
+    // , UPLOAD_DIR: path.resolve(__dirname, [UPLOAD_DIR])
 
-    , DEFAULT_USER_PASS: '[DEFAULT_USER_PASS]'
+    , DEFAULT_USER_PASS: [DEFAULT_USER_PASS]
     , ADMIN: {
-        NAME: '[ADMIN_NAME]'
-        , EMAIL: '[ADMIN_EMAIL]'
-        , PASS: '[ADMIN_PASSWORD]'
-        , SITE: '[YOUR_SITE]'
+        EMAIL: [YOUT_EMAIL]
+        , PASS: [YOUR_PASS]
+        , NAMES: [nicks array as you like]
+        , SITE: [SITE URL]
     }
     , DEFAULT_AVATAR: '/style/images/avatar.png'
 
+    // social oauth is close now
+    // , SOCIAL_AUTH_INFO: {
+    //     APPID: [APPID]
+    //     , API_KEY: [API_KEY]
+    //     , SCRIPT_KEY: [SCRIPT_KEY]
+    // }
+
+    // pager limit
     , ITEM_PER_PAGE: 20
+
+    // ajax return code
     , RCODES: {
         SUCCESS: 0
-        , AUTH_ERROR: 10000
-        , PARAM_MISSING: 10001
-        , PARAM_ILLEGAL: 10002
-        , DB_ERROR: 10003
     }
 }
